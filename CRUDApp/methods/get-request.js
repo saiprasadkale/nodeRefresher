15a2ba5152f729fb6a639ca4926b6df66df9d633
saiprasadkale <saiprasadkale@gmail.com>
2024-08-28 11:50:35 +0530

    27Aug Basic CRUD node project and reorganisation

---
+++ CRUDApp/methods/get-request.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+    if(req.url === "/api/movies"){
+        res.statusCode = 200;
+        res.setHeader("Content-Type","application/json");
+        res.write(JSON.stringify(req.movies))
+        res.end()
+    }else{
+        res.writeHead(404,{"Content-Type":"application/json"})
+        res.end(
+          JSON.stringify({ title: "Not found !", message: "Route not found." })
+        );
+    }
+};
