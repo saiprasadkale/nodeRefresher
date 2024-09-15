3e914718306e6fca5abdb812695e829d1f22f5d5
saiprasadkale <saiprasadkale@gmail.com>
2024-09-15 18:40:27 +0530

    15Sept post request in progress, body parser added.

--- CRUDApp/methods/post-request.js
+++ CRUDApp/methods/post-request.js
@@ -1,1 +1,13 @@
-module.exports = (req, res) => {};
+const requestBodyParser = require("../util/body-parser");
+const crypto = require("crypto");
+
+module.exports = async (req, res) => {
+  if (req.url === "/api/movies") {
+    try {
+      let body = await requestBodyParser(req);
+      body.id = crypto.randomUUID();
+      req.movies.push(body);
+      console.log("Request body: ", body);
+    } catch (err) {}
+  }
+};
